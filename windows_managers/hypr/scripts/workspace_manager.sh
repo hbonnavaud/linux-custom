c473f9750a7d826f0b4b8b0cf2a08861bc0df861
hbonnavaud <hbonnavaud@gmail.com>
2025-07-09 19:14:36 +0200

    .

--- windows_managers/hypr/scripts/workspace_manager.sh
+++ windows_managers/hypr/scripts/workspace_manager.sh
@@ -5,11 +5,15 @@
 
 # Enable debugging
 set -e
+ENABLE_LOG_FILE="false"
 LOG_FILE="$HOME/.config/hypr/workspace_nav.log"
 
 log_message() {
-    # exit 0  # Comment this to enable debugging
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
+    if [ $ENABLE_LOG_FILE = "true" ];
+    then
+        # exit 0  # Comment this to enable debugging
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
+    fi
 }
 
 get_current_workspace() {
